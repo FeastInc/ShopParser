f8371d5efb4f7e06a88296f6566981fa604dbc23
daniel55411 <zhe.dan28@gmail.com>
2017-06-24 10:07:51 +0500

    Добавьте файлы проекта.

---
+++ DataParser/Parser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace DataParser
+{
+    class Parser
+    {
+        public IEnumerable<ProductCategoryObject> GetProductsAndCategory(string url)
+        {
+            var stack = new Stack<List<string>>();
+            stack.Push(new List<string> {url,});
+            while (stack.Count != 0)
+            {
+                // var result = 
+                yield return new ProductCategoryObject();
+            }
+        }
+
+        public ProductCategoryObject ParseCategoryObject(HtmlDocument htmlDoc)
+        {
+            return new ProductCategoryObject();
+        }
+
+        public ProductCategoryObject ProccessURL(string url)
+        {
+            var web = new HtmlWeb();
+            var htmlDoc = web.Load(url);
+            return new ProductCategoryObject();
+        }
+    }
+}
---
+++ DataParser/ProductCategoryObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataParser
+{
+    class ProductCategoryObject
+    {
+        private List<string> Properties { get; }
+
+        public ProductCategoryObject(List<string> properties)
+        {
+            Properties = properties;
+        }
+
+        public ProductCategoryObject() { }
+
+        public ProductCategoryObject(string properties, char delemiter): 
+
+    }
+}
