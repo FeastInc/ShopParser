7a081d10583b394bf6094fc86b16db14b31074f7
daniel55411 <zhe.dan28@gmail.com>
2017-07-09 21:44:39 +0500

    Add new Parse Examples

--- DataParser/Program.cs
+++ DataParser/Program.cs
@@ -19,7 +19,7 @@ namespace DataParser
             //{
             //    Console.WriteLine(o.ToString());
             //}
-            IgrRuExample.Parse();
+            GratwestExample.Parse();
         }
     }
 }
