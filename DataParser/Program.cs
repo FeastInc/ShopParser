8d2d0a2196cd571522c144d88ed6cd307f2b429b
FeastInc <feast.inc.original@gmail.com>
2017-07-11 18:30:03 +0500

    Added DynaTone[NEED FIX], Masteras

--- DataParser/Program.cs
+++ DataParser/Program.cs
@@ -15,11 +15,7 @@ namespace DataParser
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            PlaydoradoExample.Parse();
-=======
-            GeoContExample.Parse();
->>>>>>> 79bb74200cf2cf9809f099f209a2cae4987b5a68
+            MasterasExample.Parse();
         }
     }
 }
